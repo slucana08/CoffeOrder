55856316483e2671542a1a52ecc8f231c8c01790
slucana08 <slucana0891@gmail.com>
2018-10-05 21:21:52 -0500

    Initial commit

---
+++ app/src/main/java/com/kotlin/order/coffee/coffeorder/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.kotlin.order.coffee.coffeorder
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun ordenarTazas (view: View){
+                
+    }
+}
